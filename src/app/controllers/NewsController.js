eef45d9d51c38a5a85385435557b303e5c7f33d0
The Wiz <danphm566@gmail.com>
2023-11-20 19:29:28 +0700

    Initial commit

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+// NewsController.js
+class NewsController {
+    index(req, res) {
+        res.render('news');
+    }
+
+    show(req,res){
+        res.send('News details')
+    }
+}
+
+module.exports = new NewsController;
---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,12 @@
+// NewsController.js
+class SiteController {
+    index(req, res) {
+        res.render('home');
+    }
+
+    search(req,res){
+        res.render('search')
+    }
+}
+
+module.exports = new SiteController;
