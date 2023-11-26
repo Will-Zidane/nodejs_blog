f40a2629d906c111595489795647715c18dabe0f
The Wiz <danphm566@gmail.com>
2023-11-26 16:12:05 +0700

    Resolve merge conflicts

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,11 +1,13 @@
 const Course = require('../models/Course')
-const { multipleMongooseToObject } = require('../../util/mongoose')
+const { mongooseToObject } = require('../../util/mongoose')
 class CourseController {
-
-
   // GET /course/:slug
-  show(req, res) {
-    res.send('COURSE DETAIL')
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then((course) => {
+        res.render('courses/show.hbs',{course: mongooseToObject(course)})
+      })
+      .catch(next)
   }
 }
 module.exports = new CourseController()
