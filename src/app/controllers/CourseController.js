27f7ff3fb994d41c7bbda2ca12990dfe7f0aa6b2
The Wiz <danphm566@gmail.com>
2023-11-26 14:21:32 +0700

    Resolve merge conflicts

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,11 @@
+const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose')
+class CourseController {
+
+
+  // GET /course/:slug
+  show(req, res) {
+    res.send('COURSE DETAIL')
+  }
+}
+module.exports = new CourseController()
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-const newsRouter = require('./news.route');
-const siteRouter = require('./site.route');
+const newsRouter = require('./news.route')
+const siteRouter = require('./site.route')
+const courseRouter = require('./courses.route')
 
 function route(app) {
-  app.use('/news', newsRouter);
-  app.use('/', siteRouter);
-};
+  app.use('/news', newsRouter)
+  app.use('/courses', courseRouter)
+  app.use('/', siteRouter)
+}
 
-module.exports = route;
+module.exports = route
