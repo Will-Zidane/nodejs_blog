b721402d891092b6ea9e037ef343ec9a436dcf0a
The Wiz <danphm566@gmail.com>
2023-11-27 01:08:36 +0700

    Resolve merge conflicts

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -5,9 +5,21 @@ class CourseController {
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
       .then((course) => {
-        res.render('courses/show.hbs',{course: mongooseToObject(course)})
+        res.render('courses/show.hbs', { course: mongooseToObject(course) })
       })
       .catch(next)
+  }
+  create(req, res, next) {
+    res.render('courses/create')
+  }
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+    const course = new Course(req.body)
+    course
+      .save()
+      .then(() => res.redirect('/'))
+      .catch((error) => {})
   }
 }
 module.exports = new CourseController()
